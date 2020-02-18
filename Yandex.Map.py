37fb4c7a099d4d409cf25273ab45508df3eda21f
Dmitrii-Shevchuk <dmitriishevchuk2003@yandex.ru>
2020-02-18 18:27:32 +0300

    Большая задача по Maps API. Часть №3 + доделана вторая задача

--- Yandex.Map.py
+++ Yandex.Map.py
@@ -4,31 +4,33 @@ import os
 
 map_request = 'http://static-maps.yandex.ru/1.x/'
 
-longitude = '31.268856'
-lattitude = '58.523656'
+longitude = 31.268856
+lattitude = 58.523656
 delta = 0.002
-
-map_params = {
-    'll': ','.join([longitude, lattitude]),
-    "spn": ",".join([str(delta), str(delta)]),
-    "l": "map"
-}
+foto = ''
 
 
 def map():
+    global foto
     map_params = {
-        'll': ','.join([longitude, lattitude]),
+        'll': ','.join([str(longitude), str(lattitude)]),
         "spn": ",".join([str(delta), str(delta)]),
         "l": "map"
     }
-    response = requests.get(map_request, params=map_params)
-    map = 'map.png'
-    with open(map, 'wb') as photo:
-        photo.write(response.content)
-    photo = pygame.image.load(map)
-    os.remove(map)
-    return photo
+    try:
+        response = requests.get(map_request, params=map_params)
+        map = 'map.png'
+        with open(map, 'wb') as photo:
+            photo.write(response.content)
+        photo = pygame.image.load(map)
+        os.remove(map)
+        foto = photo
 
+    except pygame.error:
+        pass
+
+
+map()
 
 pygame.init()
 
@@ -44,15 +46,32 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == 265:
-                if delta >= 0.012:
-                    delta += 0.1
-                else:
-                    delta += 0.005
+                if delta * 2 <= 66:
+                    delta *= 2
             if event.key == 259:
-                if delta > 0.112:
-                    delta -= 0.1
+                if delta / 2 >= 0.0005:
+                    delta /= 2
+
+            if event.key == pygame.K_UP:
+                if lattitude + delta <= 83:
+                    lattitude += delta
+
+            if event.key == pygame.K_DOWN:
+                if lattitude - delta >= -83:
+                    lattitude -= delta
+
+            if event.key == pygame.K_RIGHT:
+                if longitude + delta > 180:
+                    longitude = -180
                 else:
-                    if delta - 0.005 > 0:
-                        delta -= 0.005
-    screen.blit(map(), (0, 0))
+                    longitude += delta
+
+            if event.key == pygame.K_LEFT:
+                if longitude - delta < -180:
+                    longitude = 180
+                else:
+                    longitude -= delta
+            map()
+
+    screen.blit(foto, (0, 0))
     pygame.display.flip()
