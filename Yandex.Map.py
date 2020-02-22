f160393afd97aacb956dedfdf464c890ea727161
Dmitrii-Shevchuk <dmitriishevchuk2003@yandex.ru>
2020-02-22 17:43:48 +0300

    Большая задача по Maps API. Часть №9, 10

--- Yandex.Map.py
+++ Yandex.Map.py
@@ -19,20 +19,14 @@ class Ui_Form(object):
         self.lineEdit.setObjectName("lineEdit")
         self.pushButton = QtWidgets.QPushButton(Form)
         self.pushButton.setGeometry(QtCore.QRect(550, 8, 121, 34))
-        font = QtGui.QFont()
-        font.setPointSize(12)
         self.pushButton.setFont(font)
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(Form)
         self.pushButton_2.setGeometry(QtCore.QRect(550, 140, 121, 31))
-        font = QtGui.QFont()
-        font.setPointSize(12)
         self.pushButton_2.setFont(font)
         self.pushButton_2.setObjectName("pushButton_2")
         self.lineEdit2 = QtWidgets.QLineEdit(Form)
         self.lineEdit2.setGeometry(QtCore.QRect(30, 100, 650, 31))
-        font = QtGui.QFont()
-        font.setPointSize(12)
         self.lineEdit2.setFont(font)
         self.lineEdit2.setText("")
         self.lineEdit2.setObjectName("lineEdit")
@@ -63,12 +57,13 @@ class MyWidget(QWidget, Ui_Form):
         self.setupUi(self)
         self.pushButton.clicked.connect(self.search)
         self.pushButton_2.clicked.connect(self.clean)
+        self.comboBox.currentTextChanged.connect(self.change_postal_index)
 
     def search(self):
         global longitude, lattitude, pt, pt_coord
-        address = self.lineEdit.text()
+        self.current_address = self.lineEdit.text()
         request = 'https://geocode-maps.yandex.ru/1.x/?format=json' \
-            f'&apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={address}&results=1'
+            f'&apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={self.current_address}&results=1'
         response = requests.get(request)
         try:
             json_response = response.json()
@@ -76,7 +71,11 @@ class MyWidget(QWidget, Ui_Form):
                                    ['featureMember'][0]['GeoObject']['metaDataProperty']
                                    ['GeocoderMetaData']['AddressDetails']['Country']['AddressLine'])
 
+            self.postal_code = json_response['response']['GeoObjectCollection']['featureMember'][0]\
+            ['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['postal_code']
+
             position = json_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+
             longitude = float(position.split()[0])
             lattitude = float(position.split()[1])
             pt = True
@@ -85,12 +84,21 @@ class MyWidget(QWidget, Ui_Form):
         except Exception:
             self.lineEdit2.setText('Что-то пошло не так(')
 
+    def change_postal_index(self):
+        if self.comboBox.currentText() == 'С почтовым индексом':
+            self.lineEdit2.setText(self.lineEdit2.text() + ', ' + self.postal_code)
+        else:
+            self.lineEdit2.setText(self.lineEdit2.text()[:-7])
+
     def clean(self):
         global pt
         self.lineEdit.setText('')
         self.lineEdit2.setText('')
         self.comboBox.setCurrentIndex(0)
+        self.current_address = ''
+        self.postal_code = ''
         pt = False
+
 
 
 map_request = 'http://static-maps.yandex.ru/1.x/'
@@ -131,6 +139,8 @@ SIZE = [600, 450]
 font = pygame.font.Font(None, 20)
 screen = pygame.display.set_mode(SIZE)
 screen.fill((255, 255, 255))
+
+#Верхнее меню
 text = font.render('Схема', 1, (0, 0, 0))
 text_x, text_y = 425, 10
 screen.blit(text, (text_x, text_y))
@@ -145,6 +155,7 @@ text = font.render('Поиск', 1, (0, 0, 0))
 text_x, text_y = 57, 14
 screen.blit(text, (text_x, text_y))
 
+#Окно с поиском
 app = QApplication(sys.argv)
 ex = MyWidget()
 
